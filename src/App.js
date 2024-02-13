e2d25a13bb23ee6b549ba4568512ebacfa5434c9
Susu-spec <suleimansuwaibat@gmail.com>
2024-02-13 14:10:28 +0100

    Update: Osc component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import './App.scss';
 
 let actx = new AudioContext();
 let out = actx.destination;
-
-
 let osc1 = actx.createOscillator();
 let gain1 = actx.createGain();
 
@@ -13,21 +11,23 @@ osc1.connect(gain1);
 gain1.connect(out);
 
 function App() {
-const [frequency, setFrequency] = useState(osc1.frequency.value);
-const [detuneValue, changeDetune] = useState(osc1.detune.value);
-const changeOsc1Freq = (e) => {
-    let { value } = e.target;
-    setFrequency(value);
-    osc1.frequency.value = value;
-};
+const [osc1settings, setOsc1settings] = useState({
+  frequency: osc1.frequency.value,
+  detune: osc1.detune.value,
+  type: osc1.type
+})
 
-const changeOsc1Detune = (e) => {
-  let {value} = e.target;
-   changeDetune(value);
-   osc1.detune.value = value;
-   console.log(value);
+const changeOsc1 = e => {
+  let {value, id} = e.target;
+  setOsc1settings({...osc1settings, [id]: value});
+  osc1[id].value = value;
 }
 
+const changeOsc1Type = (e) => {
+  let { id } = e.target;
+  osc1.type = id;
+  setOsc1settings({...osc1settings, type: id});
+}
   return (
     <div className="App">
       <div className="background"></div>
@@ -38,8 +38,11 @@ const changeOsc1Detune = (e) => {
         <button onClick={()=> osc1.start()}>Start</button>
         <button onClick={()=> osc1.stop()}>Stop</button>
       </div>
-      <Osc1 changeFreq={changeOsc1Freq} freq={frequency}
-            changeDetuneValue={changeOsc1Detune} detune={detuneValue}/>
+      <Osc1
+          change={changeOsc1}
+          settings={osc1settings}
+          changeType={changeOsc1Type}
+      /> 
     </div>
   );
 }
--- src/components/Osc1.js
+++ src/components/Osc1.js
@@ -1,31 +1,37 @@
 import React from 'react'
 
-function Osc1({changeFreq, freq, changeDetuneValue, detune}) {
+function Osc1({change, settings, changeType}) {
+  let {type, frequency, detune} = settings;
   return (
     <div className="oscillator"> 
       <div className="control">
         <h2>osc 1</h2>
         <div className="param">
-          <h3>frequency</h3>
+          <h3>Frequency</h3>
           <input 
-                value={(freq)}
-                onChange={(changeFreq)}
+                value={(frequency)}
+                onChange={(change)}
                 max="5000" 
                 type="range" 
                 id="frequency">
           </input>
         </div> 
-      </div>
-      <div className="control">
         <div className="param">
           <h3>Detune</h3>
           <input 
                 value={(detune)}
-                onChange={(changeDetuneValue)}
+                onChange={(change)}
                 type="range" 
                 id="detune">
           </input>
-        </div>   
+        </div>
+        <div className="param">
+          <h3>wave</h3>
+          <button id="sine" onClick={changeType} className={`${type === 'sine' && 'active'}`}>sine</button>
+          <button id="triangle" onClick={changeType} className={`${type === 'triangle' && 'active'}`}>triangle</button>
+          <button id="square" onClick={changeType} className={`${type === 'square' && 'active'}`}>square</button>
+          <button id="sawtooth" onClick={changeType} className={`${type === 'sawtooth' && 'active'}`}>sawtooth</button>
+        </div>
       </div>
   </div>
 
