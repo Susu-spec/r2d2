103061332a704c9eaf7b4a26d38c2435cdf3141a
Susu-spec <suleimansuwaibat@gmail.com>
2024-02-22 21:26:42 +0100

    Update: React Strict mode

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import Store from "./context/Store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Store>
       <App />
     </Store>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
