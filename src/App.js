056dd1bd928e97e605e5f8dff0203118ca49d8de
Susu-spec <suleimansuwaibat@gmail.com>
2024-02-24 11:41:45 +0100

    Update: Component styles and ordering

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import AnimatedText from './components/AnimatedText';
 import './App.scss'; 
 
 function App() {
-  const longText = "Hello, this is R2-D2. A Keyboard interface that allows you alter the sound features for each key on the keyboard.";
+  const longText = "Hello, this is R2-D2. A Keyboard interface that allows you alter sound features for each key on the keyboard.";
   return (
     <div className="App">
       <div className="background"></div>
